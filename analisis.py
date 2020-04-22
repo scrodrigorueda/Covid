81ee0525681b204ce2096f60e70c770207a67927
sc.rodrigo.rueda@gmail.com <sc.rodrigo.rueda@gmail.com>
2020-04-22 03:28:52 -0400

    Se lograron filtrar los paises latinoAmericanos

--- analisis.py
+++ analisis.py
@@ -5,5 +5,15 @@ data = pd.read_csv("time_series_covid19_confirmed_global.csv")
 print(data.head())
 data.drop(["Province/State","Lat","Long"], axis =1, inplace = True)
 print(data.head())
-nuevo = (data.Country/Region == 'Bolivia')
-print(nuevo.head())
+LA = (
+(data['Country/Region'] == 'Argentina')|
+(data['Country/Region'] == 'Bolivia')|
+(data['Country/Region'] == 'Brazil')|
+(data['Country/Region'] == 'Chile')|
+(data['Country/Region'] == 'Colombia')|
+(data['Country/Region'] == 'Paraguay')|
+(data['Country/Region'] == 'Peru')|
+(data['Country/Region'] == 'Uruguay')
+)
+data = data[LA]
+print(data.tail())
